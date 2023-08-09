75c6e1e50850317848e5bb8f7ce422ed269bf771
lightlight98 <light0425@gmail.com>
2023-08-09 18:37:15 +0900

    aaaa

--- Introduce_card/Assets/scripts/gameManager.cs
+++ Introduce_card/Assets/scripts/gameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public class gameManager : MonoBehaviour
 {
@@ -15,49 +16,42 @@ public class gameManager : MonoBehaviour
     public GameObject nameTxt;
     public AudioClip match;
     public AudioSource audioSource;
-    public GameObject endPanel;
 
-    public Text maxScoreTxt;
-    public Text remainTimeTxt;
-    public Text matchScoreTxt;
-    public Text matchCount;
+    public GameObject fail;
+    public GameObject MainTxt;
+    public GameObject menuSet;
 
-    private Animator animator;
-    float maxScore = 0f;
-    int checkMatched = 0;
-    int count = 0;
+    private Sprite[] sprites;
+
+
+
 
     void Awake()
     {
         I = this;
     }
 
-    float time = 60.0f;
+    float time = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        int[] cardImg = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        Time.timeScale = 1.0f;
+        //int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
 
-        cardImg = cardImg.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
-        animator = timeTxt.GetComponent<Animator>();
-
-        for (int i = 0; i < 16; i ++)
+        sprites = Resources.LoadAll<Sprite>("file");
+        sprites = sprites.OrderBy(spirtes => Random.Range(-1.0f, 1.0f)).ToArray();
+        for (int i = 0; i < 16; i++)
         {
             GameObject newCard = Instantiate(card);
-            //newcard를 cards 안으로 옮김
             newCard.transform.parent = GameObject.Find("cards").transform;
-
-            float x = (i / 4) * 1.4f -2.1f;
-            float y = (i % 4) * 1.4f -3.0f;
+            float x = (i / 4) * 1.4f - 2.1f;
+            float y = (i % 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(x, y, 0);
-
-            string imgName = "img" + cardImg[i].ToString();
-            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(imgName);
+            //string rtanName = "rtan" + rtans[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = sprites[i];
         }
-
-        Time.timeScale = 1.0f;
 
 
     }
@@ -65,60 +59,78 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        time -= Time.deltaTime;
+        time += Time.deltaTime;
         timeTxt.text = time.ToString("N2");
-        if (time <= 0.0f)
+
+        if (time > 60f)
         {
-            time = 0;
-            GameEnd();
+            endTxtBox.SetActive(true);
+            Time.timeScale = 0f;
         }
-        if (time <= 10.0f)
+        if (Input.GetButtonDown("Cancel"))
         {
-            audioManager.i.audioSource.pitch = 1.1f;
+
+            menuSet.SetActive(true);
+            Time.timeScale = 0f;
+            AudioListener.pause = true;
+
         }
-        matchCount.text = count.ToString();
     }
 
     public void isMatched()
     {
-        string firstCardImage = firstCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
-        string secondCardImage = secondCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        string firstCardImage = firstCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name.TrimEnd(' ', '1');
+        string secondCardImage = secondCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name.TrimEnd(' ', '1');
+
+        MainTxt.GetComponent<Text>().text = Regex.Replace(firstCardImage, @"[\d\s]", "");
 
         if (firstCardImage == secondCardImage)
         {
             audioSource.PlayOneShot(match);
             firstCard.GetComponent<card>().destroyCard();
             secondCard.GetComponent<card>().destroyCard();
+            MainTxt.SetActive(true);
+            Invoke("closeTxt", 1f);
 
-            checkMatched += 1;
 
             int cardsLeft = GameObject.Find("cards").transform.childCount;
             if (cardsLeft == 2)
             {
-                GameEnd();
+                Time.timeScale = 0f;
+                endTxtBox.SetActive(true);
             }
         }
         else
         {
-            time -= 3.0f;
-            animator.SetTrigger("isMinus");
             firstCard.GetComponent<card>().closeCard();
             secondCard.GetComponent<card>().closeCard();
-        }
+            fail.SetActive(true);
+            Invoke("closefail", 1f);
 
+        }
         firstCard = null;
         secondCard = null;
-        count += 1;
     }
 
     void GameEnd()
     {
-        maxScore = (time / 15f + checkMatched) * 100;
-        remainTimeTxt.text = time.ToString("N2");
-        matchScoreTxt.text = checkMatched.ToString();
-        maxScoreTxt.text = maxScore.ToString("N2");
-        endPanel.SetActive(true);
+        endTxtBox.SetActive(true);
         Time.timeScale = 0.0f;
+    }
+    public void closefail()
+    {
+        fail.SetActive(false);
+    }
+    public void closeTxt()
+    {
+        MainTxt.SetActive(false);
+    }
+    public void restart()
+    {
+        menuSet.SetActive(false);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+
     }
 
 
--- Introduce_card/Assets/scripts/startBtn.cs
+++ Introduce_card/Assets/scripts/startBtn.cs
@@ -23,6 +23,6 @@ public class startBtn : MonoBehaviour
     public void startGame()
     {
         SceneManager.LoadScene("MainScene");
-        
+        AudioListener.pause = false;
     }
 }
